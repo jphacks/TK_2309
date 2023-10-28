2480de40e33c95ef70734331778ca6a5078c1c43
ryoyasuda-0211 <148039751+ryoyasuda-0211@users.noreply.github.com>
2023-10-28 13:40:22 +0900

    ひらがな化関数を追加

--- similarity_points.py
+++ similarity_points.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 # テキスト類似度関数
 def text_pair_similarity(goo_lab_api, text1, text2, request_id=None):
@@ -27,3 +28,30 @@ def text_pair_similarity(goo_lab_api, text1, text2, request_id=None):
     points = round(score * 100, 1)
 
     return points
+
+
+# ひらがな化関数
+def hiragana_conversion(goo_lab_api, sentence, output_type="hiragana", request_id=None):
+    # APIのエンドポイントURL
+    url = "https://labs.goo.ne.jp/api/hiragana"
+    
+    # リクエストボディの作成
+    payload = {
+        "app_id": goo_lab_api,
+        "sentence": sentence,
+        "output_type": output_type
+    }
+    if request_id:
+        payload["request_id"] = request_id
+    
+    # APIをコール
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    
+    # レスポンスからひらがな化結果を抽出
+    data = response.json()
+    result = data["converted"]
+    
+    return result
