92af0d0a3af0d0815b547a36a0b133d93f152753
shoma0321 <83398586+shoma0321@users.noreply.github.com>
2023-10-28 15:58:52 +0900

    Usersクラスとinit関数の作成

--- users.py
+++ users.py
@@ -12,3 +12,25 @@ day_str = now.strftime("%d")
 # 今月のポイント2倍デー
 double_days = ["1", "6", "12", "21", "2"]
 
+class Users:
+
+    USER_TABLE_NAME = os.getenv("USER_TABLE_NAME")
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(USER_TABLE_NAME)
+    line_user_id = ''
+
+    data = {}
+  
+    def __init__(self, line_user_id):
+        self.line_user_id = line_user_id
+
+        # ユーザ情報を取得
+        user = self.get_user()
+
+        # 新規ユーザの場合はデータベースに登録
+        if not 'Item' in user:
+            self.__create_user()
+
+        # 登録済みの場合
+        else:
+            self.data = user['Item']
