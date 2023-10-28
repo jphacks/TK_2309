c89977a7db8d8fceda00d3d4e9720c8b75ec21d6
ryoyasuda-0211 <148039751+ryoyasuda-0211@users.noreply.github.com>
2023-10-28 16:02:59 +0900

    ChatGPTの応答を取得する関数を追加

--- openai_handler.py
+++ openai_handler.py
@@ -13,3 +13,32 @@ openai.api_key = os.environ["OPENAI_SECRETKEY"]
 with open("configs.yml", "r") as f:
     configs = yaml.safe_load(f)
 gpt_model = configs["openai"]["gpt_model"]
+
+def send_to_openai(chat_history):
+    logger.info(chat_history)
+    payload = {
+        "model": gpt_model,
+        "messages": chat_history
+    }
+    headers = {
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer ' + openai.api_key
+    }
+    openai_api_url = "https://api.openai.com/v1/chat/completions"
+
+    try:
+        response = requests.post(openai_api_url, headers=headers, data=json.dumps(payload))
+        response_json = response.json()
+
+        response_from_gpt = openai.ChatCompletion.create(
+            model=gpt_model,
+            messages=chat_history,
+        )
+        answer_from_gpt = response_from_gpt.choices[0]["message"]["content"]
+        # print(response_from_gpt)
+        text = answer_from_gpt
+
+    except Exception as e:
+        logger.warning(e)
+        text = "error"
+    return text
