d526dd93bf90a4b7d62253568540727cad6157c8
shoma0321 <83398586+shoma0321@users.noreply.github.com>
2023-10-28 13:39:43 +0900

    user情報の取得とフレーズ設定の記述追加

--- lambda_function.py
+++ lambda_function.py
@@ -13,3 +13,18 @@ def lambda_handler(event, context):
         text = "メッセージはテキストで送信してください"
         send_reply_message(message_event['replyToken'], text)
         return {'statusCode': 200, 'body': json.dumps('Error!')}
+
+    # 重要な情報を変数に格納
+    reply_token = body['events'][0]['replyToken']
+    user_message = body['events'][0]['message']['text']
+    print(user_message)
+    line_user_id = body['events'][0]['source']['userId']
+    
+    # オブジェクトを生成
+    # リクエストを取得
+    body = json.loads(event['body'])
+    # ユーザ情報
+    user = users.Users(body['events'][0]['source']['userId'])
+
+    phrase = configs["questions"][int(day_str)]["phrase"]
+    explanation = configs["questions"][int(day_str)]["explanation"]
