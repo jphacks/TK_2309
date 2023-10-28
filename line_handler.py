cbd8e8a56f417c68ee073e6eda91de5f634ad790
shoma0321 <83398586+shoma0321@users.noreply.github.com>
2023-10-28 17:31:51 +0900

    リプライ関数

--- line_handler.py
+++ line_handler.py
@@ -10,3 +10,13 @@ from linebot.models import (
 # 環境変数からアクセストークンを取得
 LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+# 通常のリプライ
+def send_reply_message(reply_token, text):
+    try:
+        line_bot_api.reply_message(
+            reply_token,
+            TextSendMessage(text=text,)
+        )
+    except LineBotApiError as e:
+        print(f"Error: {e}")
